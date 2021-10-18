9ba80eab8281784741acee0ff3a9e73ee4b4e849
clement garcia <clix.mods.cod@gmail.com>
2021-10-18 21:56:15 +0200

    Last exo

---
+++ 02 - LesInstructionsConditionnelles/Exercice 5 Program/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Exercice_5_Program
+{
+    /*
+    Exo 5 : Saison
+    Ecrire un programme qui test une variable string contenant un mois, et qui affiche la saison correspondante.
+
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ECRIT UN MOIS EN ANGLAIS OU FRANCAIS ET LA SAISON SERA DONNNEEEERRR ( pas daccent svp ) ");
+            // Fr ou anglais, normalement c'est langlais qui est privilégié mais du coup j'ai mi les deux.
+            string aMonth = Console.ReadLine().ToUpper(); // Comme ca on évite de faire trop de case inutile
+
+            switch(aMonth)
+            {
+                case "DECEMBRE":
+                case "JANVIER":
+                case "FEVRIER":
+                case "DECEMBER":
+                case "JANUARY":
+                case "FEBRUARY":
+                Console.WriteLine("On est au mois "+aMonth+" à la saison d'hiver ");
+                break;
+                case "MARS":
+                case "AVRIL":
+                case "MAI":
+                case "MARCH":
+                case "APRIL":
+                case "MAY":
+                Console.WriteLine("On est au mois "+aMonth+" à la saison du printemps");
+                break;
+                case "JUIN":
+                case "JUILLET":
+                case "AOUT":
+                case "JUNE":
+                case "JULY":
+                case "AUGUST":
+                Console.WriteLine("On est au mois "+aMonth+" à la saison d'été");
+                break;
+                case "SEPTEMBRE":
+                case "OCTOBRE":
+                case "NOVEMBRE":
+                case "SEPTEMBER":
+                case "OCTOBER":
+                case "NOVEMBER":
+                Console.WriteLine("On est au mois "+aMonth+" à la saison d'automne ");
+                break;
+                default:
+                    Console.WriteLine("Le mois saisi est incorrect, attention il doit être inscrit sans accent");
+                break;
+
+            }
+        }
+    }
+}
---
+++ 02 - LesInstructionsConditionnelles/Exercice 6 Program/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Exercice_6_Program
+{
+
+    /*
+        Exo 6 :
+        Si l’année A n’est pas divisible par 4, alors elle n’est pas bissextile Si A est divisible par 4, l’année est bissextile sauf si
+        A est divisible par 100 et pas par 400.
+        Exemples :
+        – 1901 n’est pas bissextile car non divisible par 4
+        – 2004 est bissextile car divisible par 4 et pas par 100
+        – 2100 n’est pas bissextile car divisible par 4, divisible par 100 mais pas par 400
+        – 2000 est bissextile car divisible par 4, par 100 et par 400
+        Ecrire un programme qui détermine si une année est bissextile ou non.
+
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Saisi une année stp, je vais te dire des choses dessus");
+            string strYear = Console.ReadLine();
+            int intYear;
+            if(int.TryParse(strYear,out intYear))
+            {
+                if(intYear % 4 == 0 && intYear % 100 != 0 && intYear % 400 == 0 ) // est divisible par 4 
+                {
+                    Console.WriteLine( strYear+" est bissextile");
+                }
+                else
+                {
+                    Console.WriteLine( strYear+" n’est pas bissextile");
+                }
+               // if(intYear % 100 == 0) // est divisible par 100 
+                //if(intYear % 400 == 0) // est divisible par 400 
+            }
+            else
+            {
+                Console.WriteLine("Error");
+                
+            }
+            
+        }
+    }
+}
