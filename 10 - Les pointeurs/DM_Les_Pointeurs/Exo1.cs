a45b7a6ac974d7fd550f1d16b3522cd41f66109b
clixmods <clix.mods.cod@gmail.com>
2022-01-24 20:34:16 +0100

    DM UPDATE FINAL VERSION

--- 10 - Les pointeurs/DM_Les_Pointeurs/Exo1.cs
+++ 10 - Les pointeurs/DM_Les_Pointeurs/Exo1.cs
@@ -1,20 +1,3 @@
-/*
-================== Exo 1 - Char Pointer =====================
-
-1) Creer les variables suivantes :
-- un char caractere1 intialisé avec 'O'
-- un char caractere2 intialisé avec 'A'
-- un pointeur de type char qui reference la variable caractere1
-- un pointeur de type char qui reference la variable caractere2
-
-2) Creer une fonction qui prend deux pointeurs de type char en entrée.
-La fonction doit copier la valeur de la variable pointée par le premier pointeur dans la variable pointée par le deuxième.
-
-3) Appeler la fonction avec comme paramètres les deux pointeurs déclarés dans 1).
-
-4) Afficher les valeurs de caractere1 et caractere2 via les pointeurs.
-Le résultat doit être 'O' et 'O'.
-*/
 
 using System;
 
@@ -29,13 +12,13 @@ namespace DM_Les_Pointeurs
             char* pointChar1 = &caractere1;
             char* pointChar2 = &caractere2;
 
-            functionOne(pointChar1, pointChar2);
-
+            CopyPaste(pointChar1, pointChar2);
+             //Le résultat doit être 'O' et 'O'.
             Console.WriteLine($" Value of {nameof(pointChar1)} = {*pointChar1}");
             Console.WriteLine($" Value of {nameof(pointChar2)} = {*pointChar2}");
         }
-        //La fonction doit copier la valeur de la variable pointée par le premier pointeur dans la variable pointée par le deuxième.
-        static void functionOne(char* charRef1, char* charRef2)
+        //La fonction copie la valeur de la variable pointée par le premier pointeur dans la variable pointée par le deuxième.
+        static void CopyPaste(char* charRef1, char* charRef2)
         {
             *charRef2 = *charRef1;
         }
--- 10 - Les pointeurs/DM_Les_Pointeurs/Exo2.cs
+++ 10 - Les pointeurs/DM_Les_Pointeurs/Exo2.cs
@@ -1,59 +1,15 @@
-/*
-
-===================== Exo 2 - Boss And Player =====================
-
-1) Creer une structure Boss ayant les propriétés suivantes :
-- une vie sous forme d'entier (si la vie tombe à zero il est mort).
-- une protection (float) qui sera un multiplicateur des dégats reçus.
-
-2) La structure doit avoir un constructeur permettant de setter ces propriétés.
-
-3) Dans la structure Boss, creer une fonction qui verifie si le boss est mort.
-
-4) Dans la structure Boss, creer une fonction qui permet d'attribuer des dégats au boss.
-Appliquer les degats en fonction de s'il n'est pas mort et de sa protection (on multiplie les degats par la protection).
-Cette fonction doit être public.
-Cette fonction retourne le nombre réél de degats appliqués.
-
-5) Dans la structure Boss, créer une fonction qui affiche l'état du boss (vie et protection)
-
-6) Creer une structure Player ayant les propriétés suivantes :
-- un nom
-- un nombre de dégats
-- un pointeur de type Boss
-
-7) Dans la structure Player, creer une constructeur qui permet de setter ces propriétés.
-
-8) Dans la structure Player, creer une fonction 'Attack' qui permet d'appliquer des dégats au Boss pointé par la propriété de type pointeur.
-La fonction utilise les dégats de la propriété de la structure.
-La fonction affiche à chaque attaque : "Le Player {NomPlayer} a blessé le Boss de {dégats}".
-
-9) Utiliser ces deux structures pour faire un programme qui :
-- Creer une variable de type Boss.
-- Creer deux variables de type Player avec des dégats differents, et qui ont leur propriétés de pointeur Boss pointant la variable Boss créée juste avant.
-- Appliquer plusieurs fois les degats via les variables de type player.
-- Terminer le programme en affichant l'état du Boss après l'application des attaques.
-
-
-*/
 using System;
 namespace DM_Les_Pointeurs
 {
     unsafe class Exo2
     {
-        /*
-        6) Creer une structure Player ayant les propriétés suivantes :
-            - un nom
-            - un nombre de dégats
-            - un pointeur de type Boss
-        */
         public struct Player
         {
-            string name;
-            float damage;
-            Boss* boss;
-            /*
-            7) Dans la structure Player, creer une constructeur qui permet de setter ces propriétés.*/
+            string name; // un nom
+            float damage; // un nombre de dégats
+            Boss* boss; // - un pointeur de type Boss
+            
+            //Constructeur qui permet de setter les propriétés.
             public Player(string newName, float newDamage, Boss* bossTarget)
             {
                 name = newName;
@@ -62,7 +18,7 @@ namespace DM_Les_Pointeurs
             }
 
             /*
-            8) Dans la structure Player, creer une fonction 'Attack' qui permet d'appliquer des dégats au Boss pointé par la propriété de type pointeur.
+            Cette fonction permet d'appliquer des dégats au Boss pointé par la propriété de type pointeur.
             La fonction utilise les dégats de la propriété de la structure.
             La fonction affiche à chaque attaque : "Le Player {NomPlayer} a blessé le Boss de {dégats}".
             */
@@ -74,43 +30,39 @@ namespace DM_Les_Pointeurs
 
         public struct Boss
         {
-            /*
-            - une vie sous forme d'entier (si la vie tombe à zero il est mort).
-            - une protection (float) qui sera un multiplicateur des dégats reçus.
-            */
-            int health;
-            float multiplier;
+            int health; //  - une vie sous forme d'entier (si la vie tombe à zero il est mort).
+            float multiplier; //- une protection (float) qui sera un multiplicateur des dégats reçus.
 
-            //2) La structure doit avoir un constructeur permettant de setter ces propriétés.
-
+            // Constructeur permettant de setter les propriétés.
             public Boss(int newHealth, float newMultiplier)
             {
                 health = newHealth;
                 multiplier = newMultiplier;
             }
-            /*
-                3) Dans la structure Boss, creer une fonction qui verifie si le boss est mort.
-
-                4) Dans la structure Boss, creer une fonction qui permet d'attribuer des dégats au boss.
-                Appliquer les degats en fonction de s'il n'est pas mort et de sa protection (on multiplie les degats par la protection).
-                Cette fonction doit être public.
-                Cette fonction retourne le nombre réél de degats appliqués.           
-            */
-            public bool CheckIfDead()
+            
+            //fonction qui verifie si le boss est mort.
+            bool CheckIfDead()
             {
                 return health <= 0;
             }
+            /*
+                Cette fonction permet d'attribuer des dégats au boss.
+                Elle applique les degats en fonction de s'il n'est pas mort et de sa protection 
+                (on multiplie les degats par la protection).
+                Cette fonction retourne le nombre réél de degats appliqués.  
+            */
             public float doDamage(float amount)
             {
                 if(CheckIfDead())
                     return 0;
 
-                health -= (int)(amount*multiplier);
+                int damage = (int)(amount*multiplier);
+                health -= damage;
 
-                return amount*multiplier;
+                return damage;
             }
 
-            //5) Dans la structure Boss, créer une fonction qui affiche l'état du boss (vie et protection)
+            //Cette fonction affiche l'état du boss (vie et protection)
             public override string ToString()
             {
                 return "Boss health  = "+health+" with a protection multiplier = "+multiplier ;
@@ -119,20 +71,15 @@ namespace DM_Les_Pointeurs
 
         public static void Init()
         {
-            /*
-            9) Utiliser ces deux structures pour faire un programme qui :
-            - Creer une variable de type Boss.
-            - Creer deux variables de type Player avec des dégats differents, et qui ont leur propriétés de pointeur Boss pointant la variable Boss créée juste avant.
-            - Appliquer plusieurs fois les degats via les variables de type player.
-            - Terminer le programme en affichant l'état du Boss après l'application des attaques.*/
-            Boss MyBoss = new Boss(1000, 0.5f);
+            Boss MyBoss = new Boss(1000, 0.5f); // une variable de type Boss.
+            //deux variables de type Player avec des dégats differents, 
+            //et qui ont leur propriétés de pointeur Boss pointant la variable Boss créée juste avant.
             Player playerRed = new Player("Clix", 150, &MyBoss);
             Player playerBlue = new Player("Pedro", 100,&MyBoss);
 
+            // Application de degats via les variables de type player.
             playerRed.Attack();
             playerBlue.Attack();
-
-
             Console.WriteLine(MyBoss);
             playerRed.Attack();
             playerBlue.Attack();
@@ -142,7 +89,7 @@ namespace DM_Les_Pointeurs
             playerBlue.Attack();
             playerRed.Attack();
             playerBlue.Attack();
-             Console.WriteLine(MyBoss);
+            Console.WriteLine(MyBoss);
         }
     
       
